4a869b2d502b91b113e98ef784248ad2d2bd86b5
MISS MEL <melanie.noviot@gmail.com>
2019-11-05 15:11:06 +0100

    suite

--- src/View/ViewConnexion.java
+++ src/View/ViewConnexion.java
@@ -1,5 +1,67 @@
 package View;
 
+import Controller.ControllerConnexion;
+import Controller.ControllerFormulaire;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+
 public class ViewConnexion {
+    private ViewHandler vConnexion;
+    private Group root;
+    private Text titre;
+    private Label labelL, labelMDP;
+    private TextField textL;
+    private PasswordField password;
+    private Button btnConnexion;
+
+    ViewConnexion(ViewHandler vConnexion, Group root){
+        this.vConnexion = vConnexion;
+        this.root = root;
+
+        labelL = new Label("Login");
+        labelL.setFont(Font.font(20));
+        labelL.setLayoutX(250);
+        labelL.setLayoutY(350);
+        textL = new TextField();
+        textL.setLayoutX(320);
+        textL.setLayoutY(350);
+        labelMDP = new Label("M.Passe");
+        labelMDP.setFont(Font.font(20));
+        labelMDP.setLayoutX(250);
+        labelMDP.setLayoutY(450);
+        password = new PasswordField();
+        password.setLayoutX(340);
+        password.setLayoutY(450);
+
+        btnConnexion = initButton(500,750,"connexion");
+
+        initView();
+    }
+    void initView(){
+        root.getChildren().clear();
+        root.getChildren().add(titre);
+        root.getChildren().add(labelL);
+        root.getChildren().add(textL);
+        root.getChildren().add(labelMDP);
+        root.getChildren().add(password);
+        root.getChildren().add(btnConnexion);
+    }
+
+    private Button initButton(int largeur, int hauteur, String texteDuBouton) {
+        Button b = new Button();
+        b.setLayoutX(largeur);
+        b.setLayoutY(hauteur);
+        b.setText(texteDuBouton);
+        return b;
+    }
+    public void setEventsConnexion(ControllerConnexion cc) {
+        btnConnexion.setOnMouseClicked(cc);
+    }
 
 }
--- src/View/ViewHandler.java
+++ src/View/ViewHandler.java
@@ -1,9 +1,17 @@
 package View;
 
+import Controller.ControllerConnexion;
+import Controller.ControllerFormulaire;
+import Controller.ControllerProfil;
+import Model.ModelConnexion;
+import Model.ModelFormulaire;
+import Model.ModelProfil;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import javax.swing.plaf.SplitPaneUI;
 
 public class ViewHandler extends Application {
     private Stage primaryStage;
@@ -11,7 +19,10 @@ public class ViewHandler extends Application {
     private Group root;
     private ViewInscription vFormulaire;
     private ViewConnexion vConnexion;
-
+    private ViewProfile vProfile;
+    private ModelFormulaire modelFormulaire;
+    private ModelConnexion modelConnexion;
+    private ModelProfil modelProfil;
 
     public void start(Stage primaryStage){
         this.primaryStage = primaryStage;
@@ -20,19 +31,42 @@ public class ViewHandler extends Application {
         root = new Group();
         scene = new Scene(root,1200, 900);
 
+        modelFormulaire = new ModelFormulaire();
+        modelConnexion = new ModelConnexion();
+        modelProfil = new ModelProfil();
+
         vFormulaire = new ViewInscription(this, root);
-        vConnexion = new ViewConnexion();
+        vConnexion = new ViewConnexion(this, root);
+        //vProfile = new ViewProfile(this, root);
+
+        ControllerFormulaire controllerFormulaire = new ControllerFormulaire(this, modelFormulaire);
+        ControllerConnexion controllerConnexion = new ControllerConnexion(this, modelConnexion);
+        //ControllerProfil controllerProfil =new ControllerProfil(this, modelProfil);
 
         primaryStage.setTitle("TPFormulaire");
         primaryStage.setScene(scene);
-
         primaryStage.show();
     }
 
 
 
-    public static void main(String[] args) {
-        launch(args);
+    /*public Object getvGame() {
+
+    }*/
+
+    public void setEventHandlerConnexion(ControllerConnexion cc) {
+        vConnexion.setEventsConnexion(cc);
     }
+    /*public void setHandlerInscription(ControllerFormulaire cf){
+        vFormulaire.setEventsInscription(cf);
+    }*/
+   /* public void setEventHandlerProfil(ControllerProfil cp){
+        vProfile.setEventsProfil(cp);
+    }
+*/    public ViewInscription getvInscription() {
+        return vFormulaire;
+    }
+
+
 
 }
